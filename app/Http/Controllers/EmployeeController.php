15847f488f07df59d1d298f3b04cb82c67da137e
Marcin Janczak <marcin.janczak@gmail.com>
2025-08-07 08:54:18 +0200

    Dodano usuwanie pracownika w widoku tabeli pracowników

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -9,7 +9,11 @@ class EmployeeController
 {
     public function index(){
         $employees = Pracownik::all();
-
         return(view('employees.index', compact('employees')));
+    }
+    public function destroy($id){
+        $employee = Pracownik::findOrFail($id);
+        $employee->delete();
+        return redirect()->route('employees.index')->with('success', 'Pracownik został usunięty');
     }
 }
--- resources/views/employees/index.blade.php
+++ resources/views/employees/index.blade.php
@@ -3,28 +3,43 @@
 @section('content')
 <div class="container">
     <h1>Lista pracowników</h1>
-    
-    <table class="table table-striped">
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>Imię</th>
-                <th>Nazwisko</th>
-                <th>Akcje</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach($employees as $employee)
-            <tr>
-                <td>{{ $employee->id}}</td>
-                <td>{{ $employee->imie }}</td>
-                <td>{{ $employee->nazwisko }}</td>
-                <td>
-                    <a href="#" class="btn btn-sm btn-primary">Edytuj</a>
-                </td>
-            </tr>
-            @endforeach
-        </tbody>
-    </table>
+
+    <main>
+<table class="table table-striped">
+    <thead>
+        <tr>
+            <th>ID</th>
+            <th>Imię</th>
+            <th>Nazwisko</th>
+            <th>Akcje</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach($employees as $employee)
+        <tr>
+            <td>{{ $employee->id }}</td>
+            <td>{{ $employee->imie }}</td>
+            <td>{{ $employee->nazwisko }}</td>
+            <td>
+                <form action="{{ route('employees.destroy', $employee->id) }}" method="POST" class="d-inline">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit" class="btn btn-danger btn-sm" 
+                            onclick="return confirm('Czy na pewno chcesz usunąć tego pracownika?')">
+                        <i class="fas fa-trash-alt"></i> Usuń
+                    </button>
+                </form>
+                
+                {{-- <!-- Przykład przycisku edycji -->
+                <a href="{{ route('employees.edit', $employee->id) }}" class="btn btn-primary btn-sm">
+                    <i class="fas fa-edit"></i> Edytuj
+                </a> --}}
+            </td>
+        </tr>
+        @endforeach
+    </tbody>
+</table>
 </div>
+</main>
+
 @endsection
