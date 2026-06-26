17f9e85d38d0a5067b139086ca4c7f77c712a0d0
Marcin Janczak <marcin.janczak18@gmail.com>
2026-06-26 10:57:14 +0200

    Literówa fix

--- resources/views/items/index.blade.php
+++ resources/views/items/index.blade.php
@@ -8,7 +8,7 @@
     
     <div class="d-flex justify-content-between align-items-center mb-4">
         <h2 class="fw-bold mb-0">
-            <i class="fas fa-boxes text-primary me-2"></i> Zarządzanie Przdemiotami
+            <i class="fas fa-boxes text-primary me-2"></i> Zarządzanie Przdmiotami
         </h2>
 
     </div>
